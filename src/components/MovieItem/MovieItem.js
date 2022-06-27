17bdbff39fff89b64c7efcf1a62dc0cda56578b8
Stephen Womack <swomack111@gmail.com>
2022-06-26 22:51:55 -0500

    style movies list

--- src/components/MovieItem/MovieItem.js
+++ src/components/MovieItem/MovieItem.js
@@ -1,5 +1,11 @@
 import {useDispatch} from 'react-redux'
 import {useHistory} from 'react-router-dom'
+import './MovieItem.css'
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
 
 function MovieItem({movie}) {
     const history = useHistory();
@@ -10,10 +16,22 @@ function MovieItem({movie}) {
     }
 
     return (
-        <div  onClick={handleClick}>
-            <h3>{movie.title}</h3>
-            <img src={movie.poster} alt={movie.title}/>
-        </div>
+        <Card onClick={handleClick} sx={{ maxWidth:200, minWidth: 200, margin: 5, backgroundColor:'gray'}}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="250"
+          image={movie.poster}
+          alt={movie.name}
+        />
+        <CardContent sx={{backgroundColor: 'silver', height: '50px', display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center'}}>
+          <Typography gutterBottom variant="h6" component="div">
+            {movie.title}
+          </Typography>
+         
+        </CardContent>
+      </CardActionArea>
+    </Card>
     );
 }
 
